b4e6e64305533903cbce79a942af000d4c041822
Manish Shrivastava <41223158+manish6007@users.noreply.github.com>
2025-04-18 17:03:59 +0530

    Update __init__.py

--- dq_framework/__init__.py
+++ dq_framework/__init__.py
@@ -5,9 +5,11 @@ Data Quality Framework (DQF) Initialization Module
 from .config_loader import ConfigLoader
 from .glue_reader import GlueReader
 from .runner import run_quality_checks
+from .glue_runner import DQGlueRunner
 
 __all__ = [
     "ConfigLoader",
     "GlueReader",
-    "run_quality_checks"
+    "run_quality_checks",
+    "DQGlueRunner"
 ]
